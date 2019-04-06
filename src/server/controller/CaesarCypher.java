0ceee73672492e99176974ac396406bc6e0603fe
AM <ArsenicMeatball@yandex.com>
2019-04-06 11:38:48 -0600

    Server update
    Added CaesarCypher as an implement of now interface PasswordDecoder

---
+++ src/server/controller/CaesarCypher.java
@@ -0,0 +1,19 @@
+package server.controller;
+
+public class CaesarCypher implements PasswordDecoder {
+
+	@Override
+	public boolean decodePassword(String key, String toDecode) {
+	char[] decoding = new char[toDecode.length()];
+	for(int i = 0;i<decoding.length;i++){
+		decoding[i] = toDecode.charAt(i);
+		decoding[i]--;
+	}
+
+	String decoded = new String(decoding);
+	if(key.equals(decoded)){
+		return true;
+	}
+	return false;
+	}
+} 
--- src/server/controller/PasswordDecoder.java
+++ src/server/controller/PasswordDecoder.java
@@ -1,49 +1,18 @@
 package server.controller;
-/*
- * BUGS/FEATURES to fix/finish:
- * Convert to Interface and make a class that implemetns called CaesarCypherPassword. 
- */
 /**
- * Password Decoder is a class (future interface) that is meant to decode a
- * password and check if it i the same and the true password.
+ * Password Decoder is an interface that is meant to decode a
+ * password and check if it is the same and the true password.
  * 
  * @author: Jean-David Rousseau, Sean Kenny
  * @version 1.0
  * @since April 2nd 2019
  */
-public class PasswordDecoder {
-	/**
-	 * key is the correct password. 
-	 * toDecode is the password to check if it is correct. 
-	 */
-	private String key;
-	private char[] toDecode;
+public interface PasswordDecoder {
 
 	/**
-	 * c-tor assigns password and password to decode, future c-tor will be blank.
-	 * 
-	 * @param decoded
-	 * @param toDecode
-	 */
-	public PasswordDecoder(String decoded, String toDecode) {
-		this.toDecode = toDecode.toCharArray();
-		key = decoded;
-	}
-
-	/**
-	 * function decodes password for a simple caesar cypher.
+	 * function decodes password.
 	 * 
 	 * @return true if correct password
 	 */
-	public boolean decodePassword() {
-		for (int i = 0; i < toDecode.length; i++) {
-			toDecode[i]--;
-		}
-
-		String decoded = new String(toDecode);
-		if (key.equals(decoded)) {
-			return true;
-		}
-		return false;
-	}
+	public boolean decodePassword(String key, String toDecode);
 }
